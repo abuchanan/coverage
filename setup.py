a0d613154cde43f5375903240952e9ebc8021c55
Alex Buchanan <buchanae@gmail.com>
2013-07-17 20:39:42 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='coverage',
+    description='TODO',
+    long_description=open('README.md').read(),
+    version='1.0.0',
+    author='Alex Buchanan',
+    author_email='buchanae@gmail.com',
+    license='Apache',
+    py_modules=['coverage']
+)
